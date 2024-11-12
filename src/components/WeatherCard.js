bad494c72c89696c2f511d53deadc32aaced8c72
JRSkates <skatesproduction@gmail.com>
2024-11-12 14:28:12 +0000

    weather icon conditionally rendered with function

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,12 +1,33 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg"
+import rainy from "../assets/Rainy.svg"
+import cloudy from "../assets/Cloudy.svg"
+import partlyCloudy from "../assets/PartlyCloudy.svg"
 
 
 function WeatherCard(props) {
+  const getWeatherIcon = (forecast) => {
+    switch (forecast) {
+      case "Sunny":
+        return sunny;
+      case "Rainy":
+        return rainy;
+      case "Cloudy":
+        return cloudy;
+      case "Partly Cloudy":
+        return partlyCloudy;
+      default:
+        return sunny; // default icon if forecast doesn't match any case
+    }
+  };
+
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+            <img className="card-img-top" 
+            src = {getWeatherIcon(props.forecast)} 
+            alt="Card image cap" 
+            id = "icon"/>
         </div>
         <div className="card-body">
             <h3 className="card-title">{props.city}</h3>
